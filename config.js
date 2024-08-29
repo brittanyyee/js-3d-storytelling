6dd1a8df595a6923160893eff5f14f113ed30336
Brittany Yee <46659636+brittanyyee@users.noreply.github.com>
2024-08-29 09:47:16 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,160 @@
+{
+    "properties": {
+        "imageUrl": "https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg",
+        "title": "Explore Seattle",
+        "date": "",
+        "description": "Get more information on popular places near downtown Seattle",
+        "createdBy": "Brittany Yee",
+        "imageCredit": "Google",
+        "cameraOptions": {
+            "position": {
+                "x": 1333606.4655688345,
+                "y": -4651876.872792171,
+                "z": 4141531.615474558
+            },
+            "heading": 3.14159,
+            "pitch": -0.5235989999999999,
+            "roll": 6.283185307179586
+        }
+    },
+    "chapters": [
+        {
+            "focusOptions": {
+                "focusRadius": null,
+                "showFocus": false,
+                "showLocationMarker": true
+            },
+            "title": "Pike Place Market, Pike Street, Seattle, WA, USA",
+            "coords": {
+                "lat": 47.6094076,
+                "lng": -122.3418358
+            },
+            "cameraOptions": {
+                "position": {
+                    "x": -2304464.8823612076,
+                    "y": -3639416.731078324,
+                    "z": 4688477.537267169
+                },
+                "heading": 3.14159,
+                "pitch": -0.523599,
+                "roll": 0
+            },
+            "id": 0
+        },
+        {
+            "focusOptions": {
+                "focusRadius": null,
+                "showFocus": false,
+                "showLocationMarker": true
+            },
+            "title": "Space Needle, Broad Street, Seattle, WA, USA",
+            "coords": {
+                "lat": 47.6205063,
+                "lng": -122.3492774
+            },
+            "cameraOptions": {
+                "position": {
+                    "x": -2304513.259409068,
+                    "y": -3638447.5074318927,
+                    "z": 4689439.37151528
+                },
+                "heading": 3.14159,
+                "pitch": -0.523599,
+                "roll": 0
+            },
+            "id": 1
+        },
+        {
+            "focusOptions": {
+                "focusRadius": null,
+                "showFocus": false,
+                "showLocationMarker": true
+            },
+            "title": "Waterfront Park, Alaskan Way, Seattle, WA, USA",
+            "coords": {
+                "lat": 47.60704,
+                "lng": -122.3418276
+            },
+            "cameraOptions": {
+                "position": {
+                    "x": -2304556.5154566364,
+                    "y": -3639562.5986919,
+                    "z": 4688275.772237404
+                },
+                "heading": 3.14159,
+                "pitch": -0.523599,
+                "roll": 0
+            },
+            "id": 2
+        },
+        {
+            "focusOptions": {
+                "focusRadius": null,
+                "showFocus": false,
+                "showLocationMarker": true
+            },
+            "title": "Olympic Sculpture Park, Western Avenue, Seattle, WA, USA",
+            "coords": {
+                "lat": 47.6166028,
+                "lng": -122.3553167
+            },
+            "cameraOptions": {
+                "position": {
+                    "x": -2304998.3564001583,
+                    "y": -3638364.945699003,
+                    "z": 4689003.499363497
+                },
+                "heading": 3.14159,
+                "pitch": -0.523599,
+                "roll": 0
+            },
+            "id": 3
+        },
+        {
+            "focusOptions": {
+                "focusRadius": null,
+                "showFocus": false,
+                "showLocationMarker": true
+            },
+            "title": "Kerry Park, West Highland Drive, Seattle, WA, USA",
+            "coords": {
+                "lat": 47.6294692,
+                "lng": -122.3599224
+            },
+            "cameraOptions": {
+                "position": {
+                    "x": -2304756.81845482,
+                    "y": -3637336.8971259193,
+                    "z": 4690032.576626656
+                },
+                "heading": 3.14159,
+                "pitch": -0.523599,
+                "roll": 0
+            },
+            "id": 4
+        },
+        {
+            "focusOptions": {
+                "focusRadius": null,
+                "showFocus": false,
+                "showLocationMarker": true
+            },
+            "title": "Seattle Ferry Terminal, Seattle, WA, USA",
+            "coords": {
+                "lat": 47.6023147,
+                "lng": -122.3378362
+            },
+            "cameraOptions": {
+                "position": {
+                    "x": -2304512.1484609563,
+                    "y": -3640053.5418048673,
+                    "z": 4687924.8578764405
+                },
+                "heading": 3.14159,
+                "pitch": -0.523599,
+                "roll": 0
+            },
+            "id": 5
+        }
+    ]
+}
